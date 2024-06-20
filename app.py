2ad28e576dd97618d96f49d9ffc66d9fc9614a9f
Sourabh7iwari <Sourabh7iwari@gmail.com>
2024-06-21 01:17:06 +0530

    added redirction feature in app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask,redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
     return("Hello this is a main page <h1>HELLO This is a first heading<h1>")
 
-@app.route("/<name>")
+@app.route("/welcome/<name>")
 def user(name):
-    return f"hello {name}!"
+    return f"<h1>HELLO {name}! you're wellcome to this page.<h1>"
+
+@app.route("/newuser")
+def newuser():
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     print("Starting the flask application")
