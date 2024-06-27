59b2e94505f621a40ef44c4ab07aab4749b90c7e
Sourabh Tiwari <Sourabh7iwari@gmail.com>
2024-06-27 19:11:45 +0530

    corrected the document

--- features.py
+++ features.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 feature = Blueprint("feature", __name__, static_folder='static', template_folder='templates')
 
-#page for showing python script in html with iteratable variables
+#page for showing agian home page but with different route('features/view') in blueprint py script.
 @feature.route('/view')
 def iterate():
     return render_template("home.html")
